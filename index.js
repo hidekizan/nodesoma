1696da3c8276788d4fd88d5c48d609d8417af8df
MATHEUS HIDEKI KIYONO <matheus.hkiyono@senacsp.edu.br>
2025-04-29 21:43:38 -0300

    dale

---
+++ index.js
@@ -0,0 +1,14 @@
+var readlineSync = require('readline-sync');
+
+
+console.log("Programa que soma 10 numeros");
+let n = 1;
+let i = 1;
+let total = 0;
+while(n!=0){
+    n = parseFloat(readlineSync.question(`Informe o ${i} numero: `));
+    total = total + n;
+    i++;
+}
+
+console.log(`Total = ${total}`);
